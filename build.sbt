fe742cc2b371f379967830fde74655b9f332f536
vikas gautam <vigautam@deloitte.de>
2018-07-29 23:11:38 +0200

    Initial commit - just the skeleton

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "structured-streaming-with-kafka"
+
+version := "0.1"
+
+scalaVersion := "2.11.8"
---
+++ src/main/scala/com/wordpress/technicado/NumPrintouts.scala
@@ -0,0 +1,8 @@
+package com.wordpress.technicado
+
+object NumPrintouts {
+  def main(args: Array[String]): Unit = {
+    // TODO: implement print command counter from user activity log data
+  }
+
+}
